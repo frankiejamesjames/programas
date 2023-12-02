9faddcb11588f7c3b88bb603d7d95f2ddaf68730
frankiejamesjames <146567174+frankiejamesjames@users.noreply.github.com>
2023-12-02 10:00:38 +0100

    Add files via upload
    Factorial de Juan Antonio Ripoll

---
+++ Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace factorial_Juan_Antonio_Ripoll
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Introduce número:");
+            int number=Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine(Factorial(number));
+
+        }
+
+        public static int Factorial(int num)
+        {
+            if(num == 0)
+            {
+                return 1;
+            }
+            else
+            {
+                return num * Factorial(num - 1);
+            }
+            
+        }
+    }
+}
