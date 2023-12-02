985b586e45c0bc530a5b7f7825184898aaba145e
frankiejamesjames <146567174+frankiejamesjames@users.noreply.github.com>
2023-12-02 09:40:13 +0100

    Add files via upload

---
+++ Collatz.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Diagnostics.CodeAnalysis;
+
+namespace VT_Extra
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int numero;
+            do
+            {
+                Console.WriteLine("Introduce un numero: ");
+                numero = int.Parse(Console.ReadLine());
+                Collatz(numero);
+            }while (numero<=1);
+            
+            
+        }  
+
+        public static void Collatz(int numero)
+        {
+            Console.WriteLine(numero);
+            if(numero == 1)
+            {
+                return;
+            }
+
+            int siguientenumero;
+
+            if(numero%2==0) 
+            {
+                siguientenumero = numero / 2;
+            }
+            else
+            {
+                siguientenumero = (numero * 3) + 1;
+            }
+
+            Collatz(siguientenumero);
+        }
+        
+    }
+}
